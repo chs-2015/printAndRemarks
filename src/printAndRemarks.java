2ac9de8334d959a11cbf4ef0ee3f39c818fcf407
Brandon Berney <brandonberney@protonmail.com>
2015-08-18 13:29:09 -0400

    Add everything up to this point

---
+++ src/printAndRemarks.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Brandon B.
+ * Date: 8-18-15
+ * Description: Demonstrates the ability to use remarks and print output
+ */
+ 
+public class printAndRemarks {
+    
+    public /* im a beast */ static void main(String[] args) {
+    	
+    	// This is an inline remark
+    	System.out.println("Hello World!"); // This outputs "Hello World!"
+    	System.out.println("Peter picked a peck of pickled peppers.");
+    }
+}
